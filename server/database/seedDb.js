cb2c6d94547220ac2aedf05b296df4ee7aa4deb8
Matt Hong <matthew.m.hong@gmail.com>
2020-10-17 17:00:56 -0700

    cleaned up a lot of the commented out code, added in some newlines

--- server/database/seedDb.js
+++ server/database/seedDb.js
@@ -2,25 +2,6 @@ const faker = require('faker');
 const postgres = require('./postgres.js');
 const fs = require('fs');
 
-// url to images hosted on s3
-// let s3urls = {
-//   1: 'https://hrr-sdc-catdamon-photo-carousel.s3.us-east-2.amazonaws.com/1.jpg',
-//   2: 'https://hrr-sdc-catdamon-photo-carousel.s3.us-east-2.amazonaws.com/2.jpg',
-//   3: 'https://hrr-sdc-catdamon-photo-carousel.s3.us-east-2.amazonaws.com/3.png',
-//   4: 'https://hrr-sdc-catdamon-photo-carousel.s3.us-east-2.amazonaws.com/4.jpg',
-//   5: 'https://hrr-sdc-catdamon-photo-carousel.s3.us-east-2.amazonaws.com/5.jpg',
-//   6: 'https://hrr-sdc-catdamon-photo-carousel.s3.us-east-2.amazonaws.com/6.jpg',
-//   7: 'https://hrr-sdc-catdamon-photo-carousel.s3.us-east-2.amazonaws.com/7.jpg',
-//   8: 'https://hrr-sdc-catdamon-photo-carousel.s3.us-east-2.amazonaws.com/8.jpg',
-//   9: 'https://hrr-sdc-catdamon-photo-carousel.s3.us-east-2.amazonaws.com/9.jpg',
-//   10: 'https://hrr-sdc-catdamon-photo-carousel.s3.us-east-2.amazonaws.com/10.jpg',
-//   11: 'https://hrr-sdc-catdamon-photo-carousel.s3.us-east-2.amazonaws.com/11.jpg',
-//   12: 'https://hrr-sdc-catdamon-photo-carousel.s3.us-east-2.amazonaws.com/12.jpg',
-//   13: 'https://hrr-sdc-catdamon-photo-carousel.s3.us-east-2.amazonaws.com/13.jpg',
-//   14: 'https://hrr-sdc-catdamon-photo-carousel.s3.us-east-2.amazonaws.com/14.jpg',
-//   15: 'https://hrr-sdc-catdamon-photo-carousel.s3.us-east-2.amazonaws.com/15.jpg'
-// }
-
 let s3urls = {
   1: '1.jpg',
   2: '2.jpg',
@@ -40,9 +21,7 @@ let s3urls = {
 }
 
 let fakeImageSets = {};
-// Generate fake image sets out of
 for (var i = 1; i <= 15; i += 1) {
-  // generate image and lock based on i
   let fakeImageSet = [];
   var imageNum = Math.floor(Math.random() * 5) + 3;
   for (var j = 0; j < imageNum; j++) {
@@ -52,50 +31,20 @@ for (var i = 1; i <= 15; i += 1) {
   fakeImageSets[i] = fakeImageSet;
 }
 
-let fakeData = [];
-
-// TODO: need to either export data here to a different script or seed here
-// directly based on which db I am using
-
 const createFakeDataCSV = async function() {
-  // TODO: fill this out based on db
   const writeStream = fs.createWriteStream('photoCarousel.csv');
   writeStream.write('productId,name,photos\n');
-  // for (var i = 0; i < 10; i += 1) {
-  //   // Direct insert into db
-  //   // const fakeDataToAdd = await generateFakeData(i);
-  //   // await test(fakeDataToAdd);
-  // }
-
-  // writeStream version
   await generateCSVData(i, writeStream);
   writeStream.close();
 };
 
-// const generateFakeData = (index) => {
-//   let data = [];
-//   let size = 10;
-//   for (var i = 1; i <= size; i += 1) {
-//     const index = i % 15;
-//     data.push({
-//       productId: i + index * size,
-//       name: faker.commerce.productName(),
-//       photos: fakeImageSets[index]
-//     })
-//     //console.log('i: ' + i);
-//   }
-//   return data;
-// }
-
 const generateCSVData = async (index, stream) => {
   const size = 1e8;
-  // const size = 100;
   for (let i = 1; i <= size; i++) {
     const imageSetIndex = Math.ceil(Math.random() * 15);
     const productId = i;
     const name = faker.commerce.productName();
     const photos = fakeImageSets[imageSetIndex];
-    // const photos = [faker.image.imageUrl(), faker.image.imageUrl(), faker.image.imageUrl(), faker.image.imageUrl(), faker.image.imageUrl()];
     const fakeData = `${productId},${name},"{${photos}}"\n`;
     if (!stream.write(fakeData)) {
       await new Promise(resolve => stream.once('drain', resolve)).catch((err) => console.log(err));
@@ -103,7 +52,6 @@ const generateCSVData = async (index, stream) => {
   }
 }
 
-// TODO: will want to load to database in this function
 const loadDataIntoDb = async () => {
   console.log('drop table')
   await postgres.query('DROP TABLE IF EXISTS photo_carousel;')
