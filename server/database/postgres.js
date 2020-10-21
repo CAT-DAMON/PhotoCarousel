e9c5bbd27dd7e4a5cf75b88a9c523cf96420ce22
Matt Hong <matthew.m.hong@gmail.com>
2020-10-20 17:19:58 -0700

    added in cassandra script

--- server/database/postgres.js
+++ server/database/postgres.js
@@ -9,25 +9,6 @@ const client = new Client({
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
   database: process.env.PGDATABASE,
-})
-client.connect();
-
-const tableQuery = `
-CREATE TABLE photo_carousel (
-  id serial PRIMARY KEY,
-  productId int NOT NULL,
-  name varchar,
-  photos varchar[],
-);
-`
-// try {
-//   const createDB = await client.query('CREATE DATABASE sdc');
-//   const useDB = await client.query('USE DATABASE sdc');
-//   const createTable = await client.query(tableQuery);
-// } catch (err) {
-//   console.err(err.stack);
-// }
-
-
+});
 
 module.exports = client;
--- server/database/seedDb.js
+++ server/database/seedDb.js
@@ -1,8 +1,12 @@
 const faker = require('faker');
 const postgres = require('./postgres.js');
+const cassandra = require('./cassandra.js');
 const fs = require('fs');
+const dotenv = require('dotenv');
 
-let s3urls = {
+dotenv.config();
+
+const s3urls = {
   1: '1.jpg',
   2: '2.jpg',
   3: '3.png',
@@ -39,7 +43,7 @@ const createFakeDataCSV = async function() {
 };
 
 const generateCSVData = async (index, stream) => {
-  const size = 1e8;
+  const size = 1e7;
   for (let i = 1; i <= size; i++) {
     const imageSetIndex = Math.ceil(Math.random() * 15);
     const productId = i;
@@ -52,26 +56,62 @@ const generateCSVData = async (index, stream) => {
   }
 }
 
-const loadDataIntoDb = async () => {
-  console.log('drop table')
-  await postgres.query('DROP TABLE IF EXISTS photo_carousel;')
-  console.log('create table')
-  await postgres.query(`
-  CREATE TABLE photo_carousel (
-    id serial PRIMARY KEY,
-    productId int NOT NULL,
-    name varchar,
-    photos varchar[]
-  );
-  `)
-  console.log('load data')
-  await postgres.query(`
-  COPY photo_carousel(productId, name, photos)
-  FROM '/mnt/c/Users/10thl/hongmm/PhotoCarousel/photoCarousel.csv'
-  WITH DELIMITER ','
-  CSV HEADER;
-  `)
-  await postgres.end()
+if (process.env.DB === 'postgres') {
+  // Postgres version
+  const loadDataIntoDb = async () => {
+    postgres.connect();
+    console.log(`Connected to postgres on port ${process.env.PGPORT} with host ${process.env.PGHOST} for ${process.env.PGDATABASE}`)
+
+    console.log('drop table');
+    await postgres.query('DROP TABLE IF EXISTS photo_carousel;');
+    console.log('create table');
+    await postgres.query(`
+    CREATE TABLE photo_carousel (
+      id serial PRIMARY KEY,
+      productId int NOT NULL,
+      name varchar,
+      photos varchar[]
+    );
+    `);
+    console.log('load data');
+    await postgres.query(`
+    COPY photo_carousel(productId, name, photos)
+    FROM '/mnt/c/Users/10thl/hongmm/PhotoCarousel/photoCarousel.csv'
+    WITH DELIMITER ','
+    CSV HEADER;
+    `);
+    await postgres.end();
+  }
+} else if (process.env.DB === 'cassandra') {
+  // Cassandra version
+  const loadDataIntoDb = async () => {
+    await cassandra.connect();
+    console.log(`Connected to ${cassandra.hosts.length} nodes in the cluster: ${cassandra.hosts.keys().join(', ')}`);
+
+    const query = `CREATE KEYSPACE IF NOT EXISTS sdc WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'}`;
+    await cassandra.execute(query);
+
+    console.log('create table');
+    await cassandra.execute(`
+      CREATE TABLE IF NOT EXISTS sdc.photo_carousel (
+        productId int,
+        name varchar,
+        photos list<varchar>,
+        PRIMARY KEY (productId)
+      );
+    `);
+
+    console.log('clear data');
+    await cassandra.execute('TRUNCATE sdc.photo_carousel');
+
+    console.log('load data from csv using cqlsh COPY command');
+    const copyQuery = `
+      COPY sdc.photo_carousel(productId, name, photos)
+      FROM '/mnt/c/Users/10thl/hongmm/PhotoCarousel/photoCarousel.csv'
+      WITH HEADER = TRUE AND DELIMITER = ',';
+    `;
+    console.log('copyQuery: ' + copyQuery);
+  }
 }
 
 const seedData = async () => {
